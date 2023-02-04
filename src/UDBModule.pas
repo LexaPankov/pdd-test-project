687772937f51ae5b6d8e66bbd2b97ebc4051bedf
LexaPankov <123716334+LexaPankov@users.noreply.github.com>
2023-02-04 15:16:52 +0700

    Add files via upload

---
+++ src/UDBModule.pas
@@ -0,0 +1,18 @@
+unit UDBModule;
+
+interface
+
+
+  uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls, DB, ADODB, ExtCtrls, UConfigClient;
+
+  implementation
+
+
+procedure AddConnection(pass:String);
+begin
+//
+end;
+
+end.
